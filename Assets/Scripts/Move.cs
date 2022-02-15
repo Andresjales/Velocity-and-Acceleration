618f1406adf7696ff47c14e27563868fa0008afc
Andrés Álvarez <andresjales@gmail.com>
2022-02-15 10:07:35 -0500

    Wall bounce

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -6,15 +6,29 @@ public class Move : MonoBehaviour
 {
     Vector3 displacement;
     [SerializeField] Vector3 velocity;
+    [SerializeField]float xBorder, yBorder;
 
     void Update()
     {
         Moving();
+        CheckCollisions();
     }
 
     public void Moving()
     {
         displacement = velocity * Time.deltaTime;
         transform.position = transform.position + displacement;
+    }
+
+    private void CheckCollisions()
+    {
+        if(transform.position.x >= xBorder || transform.position.x <= -xBorder)
+        {
+            velocity.x = velocity.x * -1;
+        }
+        else if(transform.position.y >= yBorder || transform.position.y <= -yBorder)
+        {
+            velocity.y = velocity.y * -1;
+        }
     }
 }
